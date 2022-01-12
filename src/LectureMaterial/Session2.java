a6270ed25e7f81c3eb4917a6d32fa1171015a452
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2022-01-12 15:01:35 +0100

    Add example file from lecture session 2

---
+++ src/LectureMaterial/Session2.java
@@ -0,0 +1,39 @@
+package LectureMaterial;
+
+import java.util.Scanner;
+
+public class Session2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // variables
+        System.out.print("Hva er navnet ditt?");
+        String name = input.nextLine();
+
+        System.out.print("Hva er din alder? ");
+        int age = input.nextInt();
+
+        System.out.print("Hva er din høyde? ");
+        float height = input.nextFloat();
+
+
+        // System.out.println("Ditt navn er " + name);
+        System.out.printf("Ditt navn er %s\n", name);
+
+        //System.out.println("Din alder er " + age);
+        System.out.printf("Din høyde er %d\n", age);
+
+        // System.out.println("Din høyde er " + height + "m");
+        System.out.printf("Din høyde er %.2fm\n", height);
+
+        /*
+            Rule of thumb: When doing an arithmetic calculation between two different built-in types, the "smaller"
+            type will be converted into the "larger" type.
+
+            double > float > long long(C99) > long > short > char
+            - Yuan
+        */
+    }
+}
+
