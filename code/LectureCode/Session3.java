607f7232d8bb72997ad23cf0ff11e104945c727a
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2022-01-17 09:04:40 +0100

    Add files for session 3

---
+++ code/LectureCode/Session3.java
@@ -0,0 +1,7 @@
+package LectureCode;
+
+public class Session3 {
+    public static void main(String[] args) {
+        System.out.println("Session 3");
+    }
+}
---
+++ code/Tasks/step3/src/Wallet.java
@@ -0,0 +1,20 @@
+package Tasks.step3.src;
+
+/**
+ *  Wallet-class
+ *
+ *  Fields:
+ *  - int coins
+ *      - Amount of coins within the wallet
+ *
+ *  Methods:
+ *  - public void printCoins()
+ *      - Prints the amount of counts within the wallet to standard output
+ */
+public class Wallet {
+    int coins = 0;
+
+    public void printCoins() {
+        System.out.println("You have " + this.coins + " coins.");
+    }
+}
