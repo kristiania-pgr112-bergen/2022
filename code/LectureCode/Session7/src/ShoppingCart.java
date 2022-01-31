91c49669924a29da802c708ce57711ef8ce795cc
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2022-01-31 11:03:01 +0100

    Added comments to ShoppingCart code written during end of lecture

--- code/LectureCode/Session7/src/ShoppingCart.java
+++ code/LectureCode/Session7/src/ShoppingCart.java
@@ -2,30 +2,35 @@ package LectureCode.Session7.src;
 
 import java.util.ArrayList;
 
+
 public class ShoppingCart {
     ArrayList<ProductLine> productLines = new ArrayList<>();
 
     void addProductLine(ProductLine input) {
         // check if product line already exists within shopping cart
-
         for (ProductLine productLine : productLines) {
             Product product = productLine.getProduct();
 
+            // product line already exists
             if (product.getName().equals(input.getProduct().getName())
              && product.getPrice() == input.getProduct().getPrice()) {
                 productLine.increaseQuantity(input.getQuantity());
 
+                // return here to skip running code after the loop
                 return;
             }
         }
 
+        // if no product line that already exists, add it
         this.productLines.add(input);
     }
 
     void removeProductLine(Product input) {
+        // go through each product line to check if product given is a part of one of them
         for (int i = 0; i < this.productLines.size(); i ++) {
             Product product = this.productLines.get(i).getProduct();
 
+            // if product is found within a product line that exists, remove it
             if (product.getName().equals(input.getName())
              && product.getPrice() == input.getPrice()) {
                 this.productLines.remove(i);
@@ -38,6 +43,7 @@ public class ShoppingCart {
     float calculateTotal() {
         float output = 0.0f;
 
+        // sum the price of all products within each product line
         for (ProductLine productLine : this.productLines) {
             output += productLine.getProduct().getPrice() * productLine.getQuantity();
         }
