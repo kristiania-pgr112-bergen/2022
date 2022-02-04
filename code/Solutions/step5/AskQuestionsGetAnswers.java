2bce9e18098d87fe601df811ce11d7b045055cda
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2022-02-05 00:47:36 +0100

    Add base for step 5 solution example

---
+++ code/Solutions/step5/AskQuestionsGetAnswers.java
@@ -0,0 +1,46 @@
+package Solutions.step5;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ *  Task 5
+ *          Ask N questions and get N answers
+ *
+ *  - [ ] Ask how many questions to gather as an int
+ *  - [ ] Use a loop to ask for that many questions
+ *  - [ ] Store each question received in the questions ArrayList
+ *  - [ ] Ask each question and collect answers into the answers ArrayList
+ *  - [ ] Loop through each question and output the answer given to that question
+ *
+ *
+ *  In other words:
+ *
+ *      Ask how many questions the user should give you.
+ *      Collect that many questions.
+ *      Ask each question and collect the answer.
+ *      Display a list of each question and answer given.
+ *
+ */
+
+public class AskQuestionsGetAnswers {
+    Scanner scanner = new Scanner(System.in);
+
+    ArrayList<String> questions = new ArrayList<>();
+    ArrayList<String> answers = new ArrayList<>();
+
+    int questionCount = 0;
+
+    void addQuestion(String question) {
+
+    }
+
+    void getAnswer() {
+
+    }
+
+    public static void main(String[] args) {
+        AskQuestionsGetAnswers FAQ = new AskQuestionsGetAnswers();
+    }
+}
