e20bf4d6b5d65353149e836ce8a821730a9c6ddd
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2022-02-07 11:40:59 +0100

    Refactor example from session 2

--- code/LectureCode/Session2/ScannerExample.java
+++ code/LectureCode/Session2/ScannerExample.java
@@ -1,8 +1,8 @@
-package LectureCode;
+package LectureCode.Session2;
 
 import java.util.Scanner;
 
-public class Session2 {
+public class ScannerExample {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
