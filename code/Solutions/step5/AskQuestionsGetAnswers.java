307b74dac13857ecc5aa261b5b36a51fbd238129
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2022-02-07 11:33:48 +0100

    Add solution to step 5

--- code/Solutions/step5/AskQuestionsGetAnswers.java
+++ code/Solutions/step5/AskQuestionsGetAnswers.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
  *  Task 5
  *          Ask N questions and get N answers
  *
- *  - [x] Ask how many questions to gather as an int
- *  - [ ] Use a loop to ask for that many questions
- *  - [ ] Store each question received in the questions ArrayList
- *  - [ ] Ask each question and collect answers into the answers ArrayList
- *  - [ ] Loop through each question and output the answer given to that question
+ *  - [X] Ask how many questions to gather as an int
+ *  - [X] Use a loop to ask for that many questions
+ *  - [X] Store each question received in the questions ArrayList
+ *  - [X] Ask each question and collect answers into the answers ArrayList
+ *  - [X] Loop through each question and output the answer given to that question
  *
  *
  *  In other words:
@@ -32,14 +32,44 @@ public class AskQuestionsGetAnswers {
 
     int questionCount = 0;
 
-    void getQuestionCount() {
-        System.out.println("How many questions do you want to add?");
+    public void start() {
+        System.out.println("Hvor mange spørsmål ønsker du å ha?");
 
-        this.questionCount = this.scanner.nextInt();
+        questionCount = Integer.parseInt(this.scanner.nextLine());
+
+        this.getQuestions();
+        this.askQuestions();
+
+        this.printQuestionsAndAnswers();
     }
 
-    void start() {
-        this.getQuestionCount();
+    private void getQuestions() {
+        for (int i = 0; i < this.questionCount; i ++) {
+            System.out.println("Skriv inn spørsmålet ditt:");
+
+            questions.add(this.scanner.nextLine());
+        }
+    }
+
+    private void askQuestions() {
+        for (String question : this.questions) {
+            System.out.printf("%s%n", question);
+
+            this.answers.add(this.scanner.nextLine());
+        }
+    }
+
+    private void printQuestionsAndAnswers() {
+        System.out.println("# Friendly Questions and Answers");
+
+        if (this.questions.size() == this.answers.size()) {
+            for (int i = 0; i < this.questions.size(); i ++) {
+                System.out.printf("Q: %s%n", this.questions.get(i));
+                System.out.printf("A: %s%n", this.answers.get(i));
+
+                System.out.println("---");
+            }
+        }
     }
 
     public static void main(String[] args) {
