a3b929ed9341875f51c60e37a2021b870f6f7ed3
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2022-02-14 13:37:52 +0100

    Step 9 oppg 1

---
+++ code/Solutions/step9/Movable.java
@@ -0,0 +1,8 @@
+package Solutions.step9;
+
+public interface Movable {
+    void moveUp(double distance);
+    void moveDown(double distance);
+    void moveRight(double distance);
+    void moveLeft(double distance);
+}
